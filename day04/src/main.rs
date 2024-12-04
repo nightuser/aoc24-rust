c5237cc4cb27fcbe14b790120d5826b1fad2f62c
nightuser <nightuser.android@gmail.com>
2024-12-04 12:32:01 +0000

    [day04] Simplify and refactor
    * Add grid and point types
    * Simplify logic

--- day04/src/main.rs
+++ day04/src/main.rs
@@ -5,7 +5,11 @@ use std::{env, iter};
 
 use anyhow::anyhow;
 
-const SHIFTS: [(isize, isize); 8] = [
+type Point = (usize, usize);
+type PointShift = (isize, isize);
+type Grid<T> = HashMap<Point, T>;
+
+const SHIFTS: [PointShift; 8] = [
     (0, 1),
     (1, 1),
     (1, 0),
@@ -16,21 +20,26 @@ const SHIFTS: [(isize, isize); 8] = [
     (-1, 1),
 ];
 
-fn shift_point((x, y): (usize, usize), (dx, dy): (isize, isize)) -> (usize, usize) {
+const NEEDLE: &str = "XMAS";
+
+fn shift_point((x, y): Point, (dx, dy): PointShift) -> Point {
     (x.wrapping_add_signed(dx), y.wrapping_add_signed(dy))
 }
 
-const NEEDLE: &str = "XMAS";
+fn get_string(grid: &Grid<char>, indices: impl IntoIterator<Item = Point>) -> Option<String> {
+    indices.into_iter().map(|p| grid.get(&p)).collect()
+}
 
 fn main() -> anyhow::Result<()> {
     let input = env::args_os()
         .nth(1)
         .ok_or(anyhow!("provide an input file"))?;
     let reader = BufReader::new(File::open(input)?);
-    let mut grid: HashMap<(usize, usize), char> = HashMap::new();
+
+    let mut grid: Grid<char> = HashMap::new();
     let valid_symbols: HashSet<_> = NEEDLE.chars().collect();
-    let mut x_starts: Vec<(usize, usize)> = Vec::new();
-    let mut a_starts: Vec<(usize, usize)> = Vec::new();
+    let mut x_starts: Vec<Point> = Vec::new();
+    let mut a_starts: Vec<Point> = Vec::new();
     for (y, line) in reader.lines().enumerate() {
         let line = line?;
         let row: Vec<_> = line.chars().collect();
@@ -53,32 +62,24 @@ fn main() -> anyhow::Result<()> {
             SHIFTS
                 .into_iter()
                 .filter(|&shift| {
-                    let positions = iter::successors(Some(start), |&p| Some(shift_point(p, shift)));
-                    NEEDLE
-                        .chars()
-                        .zip(positions)
-                        .all(|(exp_c, p)| grid.get(&p).is_some_and(|&c| c == exp_c))
+                    let indices = iter::successors(Some(start), |&p| Some(shift_point(p, shift)))
+                        .take(NEEDLE.len());
+                    get_string(&grid, indices).is_some_and(|s| s == NEEDLE)
                 })
                 .count()
         })
         .sum();
     println!("ans1 = {ans1}");
 
-    let required_neighbors = HashSet::from(['M', 'S']);
     let ans2 = a_starts
         .into_iter()
         .filter(|&start| {
-            let is_valid = |shifts: &[(isize, isize)]| -> bool {
-                let result: Option<HashSet<char>> = shifts
-                    .iter()
-                    .map(|&shift| {
-                        let p = shift_point(start, shift);
-                        grid.get(&p).copied()
-                    })
-                    .collect();
-                result.is_some_and(|s| s == required_neighbors)
-            };
-            is_valid(&[(1, 1), (-1, -1)]) && is_valid(&[(1, -1), (-1, 1)])
+            [[(1, 1), (-1, -1)], [(1, -1), (-1, 1)]]
+                .into_iter()
+                .all(|shifts| -> bool {
+                    let indices = shifts.into_iter().map(|shift| shift_point(start, shift));
+                    get_string(&grid, indices).is_some_and(|s| s == "MS" || s == "SM")
+                })
         })
         .count();
     println!("ans2 = {ans2}");
