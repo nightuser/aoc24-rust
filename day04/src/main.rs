4b2bffb722fbe8229b15459a72324d038760aa3a
nightuser <nightuser.android@gmail.com>
2024-12-04 12:04:54 +0000

    [day04] Use sparse grid

--- day04/src/main.rs
+++ day04/src/main.rs
@@ -1,34 +1,9 @@
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 use std::{env, iter};
 
 use anyhow::anyhow;
-
-#[derive(Debug)]
-struct Grid<T> {
-    data: Vec<Vec<T>>,
-}
-
-impl<T> Grid<T> {
-    pub fn new() -> Self {
-        Self { data: Vec::new() }
-    }
-
-    pub fn add_row(&mut self, row: Vec<T>) {
-        self.data.push(row);
-    }
-
-    pub fn get(&self, x: usize, y: usize) -> Option<&T> {
-        self.data.get(y).and_then(|row| row.get(x))
-    }
-}
-
-impl<T> Default for Grid<T> {
-    fn default() -> Self {
-        Self::new()
-    }
-}
 
 const SHIFTS: [(isize, isize); 8] = [
     (0, 1),
@@ -45,25 +20,31 @@ fn shift_point((x, y): (usize, usize), (dx, dy): (isize, isize)) -> (usize, usiz
     (x.wrapping_add_signed(dx), y.wrapping_add_signed(dy))
 }
 
+const NEEDLE: &str = "XMAS";
+
 fn main() -> anyhow::Result<()> {
     let input = env::args_os()
         .nth(1)
         .ok_or(anyhow!("provide an input file"))?;
     let reader = BufReader::new(File::open(input)?);
-    let mut grid: Grid<char> = Grid::new();
+    let mut grid: HashMap<(usize, usize), char> = HashMap::new();
+    let valid_symbols: HashSet<_> = NEEDLE.chars().collect();
     let mut x_starts: Vec<(usize, usize)> = Vec::new();
     let mut a_starts: Vec<(usize, usize)> = Vec::new();
     for (y, line) in reader.lines().enumerate() {
         let line = line?;
         let row: Vec<_> = line.chars().collect();
-        for (x, c) in row.iter().enumerate() {
-            match *c {
+        for (x, c) in row.into_iter().enumerate() {
+            if !valid_symbols.contains(&c) {
+                continue;
+            }
+            match c {
                 'X' => x_starts.push((x, y)),
                 'A' => a_starts.push((x, y)),
                 _ => {}
-            }
+            };
+            grid.insert((x, y), c);
         }
-        grid.add_row(row);
     }
 
     let ans1: usize = x_starts
@@ -73,10 +54,10 @@ fn main() -> anyhow::Result<()> {
                 .into_iter()
                 .filter(|&shift| {
                     let positions = iter::successors(Some(start), |&p| Some(shift_point(p, shift)));
-                    "XMAS"
+                    NEEDLE
                         .chars()
                         .zip(positions)
-                        .all(|(exp_c, (x, y))| grid.get(x, y).is_some_and(|&c| c == exp_c))
+                        .all(|(exp_c, p)| grid.get(&p).is_some_and(|&c| c == exp_c))
                 })
                 .count()
         })
@@ -91,8 +72,8 @@ fn main() -> anyhow::Result<()> {
                 let result: Option<HashSet<char>> = shifts
                     .iter()
                     .map(|&shift| {
-                        let (x, y) = shift_point(start, shift);
-                        grid.get(x, y).copied()
+                        let p = shift_point(start, shift);
+                        grid.get(&p).copied()
                     })
                     .collect();
                 result.is_some_and(|s| s == required_neighbors)
