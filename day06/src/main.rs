21002f66d15ba998008c63c8790342c39d9343c4
nightuser <nightuser.android@gmail.com>
2024-12-06 23:01:07 +0000

    [day06] Optimize heavily, avoid allocations

--- day06/src/main.rs
+++ day06/src/main.rs
@@ -1,6 +1,4 @@
-use std::borrow::Cow;
 use std::cmp::Ordering;
-use std::collections::{HashMap, HashSet};
 use std::env;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
@@ -8,9 +6,16 @@ use std::iter;
 
 type Point = (usize, usize);
 type Position = (Point, Dir);
-type Obstructions<'a> = Vec<Cow<'a, [usize]>>;
+type Obstructions = Vec<Vec<usize>>;
 
-#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
+#[derive(Clone, Copy, PartialEq)]
+enum Status {
+    Unknown,
+    NotLoop,
+    Loop,
+}
+
+#[derive(Clone, Copy, Debug)]
 enum Dir {
     Up,
     Right,
@@ -19,7 +24,7 @@ enum Dir {
 }
 
 impl Dir {
-    pub fn turn_right(&self) -> Self {
+    pub fn turn_right(self) -> Self {
         match self {
             Dir::Up => Dir::Right,
             Dir::Right => Dir::Down,
@@ -27,88 +32,102 @@ impl Dir {
             Dir::Left => Dir::Up,
         }
     }
+
+    pub fn to_index(self, point_index: usize) -> usize {
+        4 * point_index + self as usize
+    }
 }
 
-fn lower_bound<'a, T: Ord>(haystack: &'a [T], needle: &'a T) -> Option<&'a T> {
+fn lower_bound<T: Ord + Clone>(haystack: &[T], needle: T, temp_bound: Option<T>) -> Option<T> {
     let index = haystack
-        .binary_search_by(|x| match x.cmp(needle) {
+        .binary_search_by(|x| match x.cmp(&needle) {
             Ordering::Equal => Ordering::Greater,
             ord => ord,
         })
         .unwrap_err();
-    if index == 0 {
-        None
-    } else {
-        Some(&haystack[index - 1])
-    }
+    let result = haystack.get(index.wrapping_sub(1)).cloned();
+    temp_bound
+        .and_then(|t| (t < needle && result.clone().is_none_or(|result| t > result)).then_some(t))
+        .or(result)
 }
 
-fn upper_bound<'a, T: Ord>(haystack: &'a [T], needle: &'a T) -> Option<&'a T> {
+fn upper_bound<T: Ord + Clone>(haystack: &[T], needle: T, temp_bound: Option<T>) -> Option<T> {
     let index = haystack
-        .binary_search_by(|x| match x.cmp(needle) {
+        .binary_search_by(|x| match x.cmp(&needle) {
             Ordering::Equal => Ordering::Less,
             ord => ord,
         })
         .unwrap_err();
-    if index == haystack.len() {
-        None
-    } else {
-        Some(&haystack[index])
+    let result = haystack.get(index).cloned();
+    temp_bound
+        .and_then(|t| (t > needle && result.clone().is_none_or(|result| t < result)).then_some(t))
+        .or(result)
+}
+
+enum GridState {
+    OutOfBounds,
+    Obstruction,
+    Point(Point),
+}
+
+struct TemporaryObstruction<'a> {
+    grid: &'a Grid,
+    temp_obstruction: Point,
+}
+
+impl TemporaryObstruction<'_> {
+    pub fn stops(&self, init_pos: Position) -> impl Iterator<Item = Position> + use<'_> {
+        let (t_x, t_y) = self.temp_obstruction;
+        iter::successors(Some(init_pos), move |&((x, y), dir)| {
+            let next = match dir {
+                Dir::Up => lower_bound(
+                    &self.grid.vert_obstructions[x],
+                    y,
+                    (x == t_x).then_some(t_y),
+                )
+                .map(|o_y| (x, o_y + 1)),
+                Dir::Right => {
+                    upper_bound(&self.grid.hor_obstructions[y], x, (y == t_y).then_some(t_x))
+                        .map(|o_x| (o_x - 1, y))
+                }
+                Dir::Down => upper_bound(
+                    &self.grid.vert_obstructions[x],
+                    y,
+                    (x == t_x).then_some(t_y),
+                )
+                .map(|o_y| (x, o_y - 1)),
+                Dir::Left => {
+                    lower_bound(&self.grid.hor_obstructions[y], x, (y == t_y).then_some(t_x))
+                        .map(|o_x| (o_x + 1, y))
+                }
+            };
+            next.map(|next| (next, dir.turn_right()))
+        })
+        .skip(1) // Skip initial position.
     }
 }
 
-struct Path<'a> {
-    grid: &'a Grid<'a>,
-    current_pos: Option<Position>,
-}
-
-impl Iterator for Path<'_> {
-    type Item = (Position, Option<Point>);
-
-    fn next(&mut self) -> Option<Self::Item> {
-        let pos = self.current_pos?;
-        let ((x, y), dir) = pos;
-        let next_point = match dir {
-            Dir::Up => (x, y.wrapping_sub(1)),
-            Dir::Right => (x.wrapping_add(1), y),
-            Dir::Down => (x, y.wrapping_add(1)),
-            Dir::Left => (x.wrapping_sub(1), y),
-        };
-        let next = if !self.grid.is_in_bounds(next_point) {
-            self.current_pos = None;
-            None
-        } else if self.grid.is_obstruction(next_point) {
-            self.current_pos = Some(((x, y), dir.turn_right()));
-            None
-        } else {
-            self.current_pos = Some((next_point, dir));
-            Some(next_point)
-        };
-        Some((pos, next))
-    }
-}
-
-struct Grid<'a> {
+struct Grid {
     width: usize,
     height: usize,
-    vert_obstructions: Obstructions<'a>,
-    hor_obstructions: Obstructions<'a>,
+    vert_obstructions: Obstructions,
+    hor_obstructions: Obstructions,
 }
 
-impl<'a> Grid<'a> {
+impl Grid {
     pub fn new<T: IntoIterator<Item = Point>>(
         width: usize,
         height: usize,
         obstructions: T,
     ) -> Self {
-        let mut vert_obstructions: Obstructions<'a> = vec![Cow::default(); width];
-        let mut hor_obstructions: Obstructions<'a> = vec![Cow::default(); height];
+        let mut vert_obstructions: Obstructions = vec![vec![]; width];
+        let mut hor_obstructions: Obstructions = vec![vec![]; height];
         for (x, y) in obstructions {
-            vert_obstructions[x].to_mut().push(y);
-            hor_obstructions[y].to_mut().push(x);
+            vert_obstructions[x].push(y);
+            hor_obstructions[y].push(x);
         }
-        vert_obstructions.iter_mut().for_each(|v| v.to_mut().sort());
-        hor_obstructions.iter_mut().for_each(|v| v.to_mut().sort());
+        vert_obstructions.iter_mut().for_each(|v| v.sort());
+        hor_obstructions.iter_mut().for_each(|v| v.sort());
         Grid {
             width,
             height,
@@ -117,20 +136,10 @@ impl<'a> Grid<'a> {
         }
     }
 
-    pub fn with_parent(parent: &'a Grid, (x, y): Point) -> Self {
-        let mut vert_obstructions = parent.vert_obstructions.clone();
-        let mut hor_obstructions = parent.hor_obstructions.clone();
-        if let Err(pos) = vert_obstructions[x].binary_search(&y) {
-            vert_obstructions[x].to_mut().insert(pos, y);
-        }
-        if let Err(pos) = hor_obstructions[y].binary_search(&x) {
-            hor_obstructions[y].to_mut().insert(pos, x);
-        }
-        Grid {
-            width: parent.width,
-            height: parent.height,
-            vert_obstructions,
-            hor_obstructions,
+    pub fn with_obstruction(&self, point: Point) -> TemporaryObstruction<'_> {
+        TemporaryObstruction {
+            grid: self,
+            temp_obstruction: point,
         }
     }
 
@@ -142,32 +151,25 @@ impl<'a> Grid<'a> {
         self.vert_obstructions[x].binary_search(&y).is_ok()
     }
 
-    pub fn path(&self, init_pos: Position) -> Path<'_> {
-        Path {
-            grid: self,
-            current_pos: Some(init_pos),
+    pub fn step(&self, (current, dir): Position) -> GridState {
+        let (x, y) = current;
+        let next = match dir {
+            Dir::Up => (x, y.wrapping_sub(1)),
+            Dir::Right => (x.wrapping_add(1), y),
+            Dir::Down => (x, y.wrapping_add(1)),
+            Dir::Left => (x.wrapping_sub(1), y),
+        };
+        if !self.is_in_bounds(next) {
+            GridState::OutOfBounds
+        } else if self.is_obstruction(next) {
+            GridState::Obstruction
+        } else {
+            GridState::Point(next)
         }
     }
 
-    pub fn stops(&self, init_pos: Position) -> impl Iterator<Item = Position> + use<'_> {
-        iter::successors(Some(init_pos), |&((x, y), dir)| {
-            let next = match dir {
-                Dir::Up => {
-                    lower_bound(self.vert_obstructions[x].as_ref(), &y).map(|&o_y| (x, o_y + 1))
-                }
-                Dir::Right => {
-                    upper_bound(self.hor_obstructions[y].as_ref(), &x).map(|&o_x| (o_x - 1, y))
-                }
-                Dir::Down => {
-                    upper_bound(self.vert_obstructions[x].as_ref(), &y).map(|&o_y| (x, o_y - 1))
-                }
-                Dir::Left => {
-                    lower_bound(self.hor_obstructions[y].as_ref(), &x).map(|&o_x| (o_x + 1, y))
-                }
-            };
-            next.map(|next| (next, dir.turn_right()))
-        })
-        .skip(1) // Skip initial position.
+    pub fn point_index(&self, (x, y): Point) -> usize {
+        y * self.width + x
     }
 }
 
@@ -193,41 +195,54 @@ fn main() {
         }
         y += 1;
     }
+    let (width, height) = (x, y);
     let start = start.unwrap();
-    let grid = Grid::new(x, y, obstructions);
+    let grid = Grid::new(width, height, obstructions);
 
-    let mut path_points: HashSet<Point> = HashSet::new();
-    let mut is_loop: HashMap<Point, bool> = HashMap::new();
-    let mut visited: HashSet<Position> = HashSet::new();
-    let mut tmp_visited: HashSet<Position> = HashSet::new();
-    for (pos, next) in grid.path((start, Dir::Up)) {
-        path_points.insert(pos.0);
-        visited.insert(pos);
+    let mut path_points = vec![false; width * height];
+    let mut is_loop = vec![Status::Unknown; width * height];
+    let mut visited = vec![false; width * height * 4];
+    let mut tmp_visited = vec![false; width * height * 4];
+    let mut current = start;
+    let mut dir = Dir::Up;
+    loop {
+        let current_index = grid.point_index(current);
+        path_points[current_index] = true;
+        let pos = (current, dir);
+        let pos_index = dir.to_index(current_index);
+        visited[pos_index] = true;
 
-        let Some(next) = next else {
-            continue;
-        };
-        if next == start || is_loop.contains_key(&next) {
-            continue;
-        }
-        let new_grid = Grid::with_parent(&grid, next);
-        let mut found = false;
-        for new_pos in new_grid.stops(pos) {
-            if visited.contains(&new_pos) || tmp_visited.contains(&new_pos) {
-                found = true;
+        let next = match grid.step(pos) {
+            GridState::OutOfBounds => {
                 break;
             }
-            tmp_visited.insert(new_pos);
+            GridState::Obstruction => {
+                dir = dir.turn_right();
+                continue;
+            }
+            GridState::Point(next) => next,
+        };
+        current = next;
+        let next_index = next.1 * width + next.0;
+        if next == start || is_loop[next_index] != Status::Unknown {
+            continue;
         }
-        tmp_visited.clear();
-        is_loop.insert(next, found);
+        let extended_grid = grid.with_obstruction(next);
+        let mut found = Status::NotLoop;
+        for (new_point, new_dir) in extended_grid.stops(pos) {
+            let new_pos_index = new_dir.to_index(grid.point_index(new_point));
+            if visited[new_pos_index] || tmp_visited[new_pos_index] {
+                found = Status::Loop;
+                break;
+            }
+            tmp_visited[new_pos_index] = true;
+        }
+        tmp_visited.fill(false);
+        is_loop[next_index] = found;
     }
 
-    let ans1 = path_points.len();
-    let ans2 = is_loop.into_values().filter(|b| *b).count();
+    let ans1 = path_points.into_iter().filter(|b| *b).count();
+    let ans2 = is_loop.into_iter().filter(|b| *b == Status::Loop).count();
     println!("ans1 = {ans1}");
     println!("ans2 = {ans2}");
 }
-
-#[cfg(test)]
-mod tests;
