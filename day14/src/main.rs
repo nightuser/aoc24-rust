c78aa09cab97d4653cc878cc9387e5c8982443e4
nightuser <nightuser.android@gmail.com>
2024-12-15 15:02:50 +0000

    [day14] Make map sizes constant

--- day14/src/main.rs
+++ day14/src/main.rs
@@ -7,14 +7,11 @@ use std::io::{BufRead, BufReader};
 use regex::Regex;
 
 type Point = (i32, i32);
+const WIDTH: i32 = 101;
+const HEIGHT: i32 = 103;
 
 fn main() {
     let input = env::args_os().nth(1).unwrap();
-    let (width, height) = if input == "in.txt" {
-        (101, 103)
-    } else {
-        (11, 7)
-    };
     let reader = BufReader::new(File::open(input).unwrap());
     let line_re = Regex::new(r"^p=([0-9-]+),([0-9-]+) v=([0-9-]+),([0-9-]+)$").unwrap();
     let mut robots: Vec<(Point, Point)> = Vec::new();
@@ -26,17 +23,16 @@ fn main() {
         robots.push(((p_x, p_y), (v_x, v_y)));
     }
 
-    let (mid_x, mid_y) = (width / 2, height / 2);
     let mut q1 = 0;
     let mut q2 = 0;
     let mut q3 = 0;
     let mut q4 = 0;
     for (p, v) in robots.iter() {
         let (x, y) = (
-            (p.0 + 100 * v.0).rem_euclid(width),
-            (p.1 + 100 * v.1).rem_euclid(height),
+            (p.0 + 100 * v.0).rem_euclid(WIDTH),
+            (p.1 + 100 * v.1).rem_euclid(HEIGHT),
         );
-        match (x.cmp(&mid_x), y.cmp(&mid_y)) {
+        match (x.cmp(&(&WIDTH / 2)), y.cmp(&(&HEIGHT / 2))) {
             (Less, Less) => q1 += 1,
             (Less, Greater) => q2 += 1,
             (Greater, Less) => q3 += 1,
@@ -47,16 +43,16 @@ fn main() {
     let ans1 = q1 * q2 * q3 * q4;
 
     let mut ans2 = 0;
-    let mut grid: Vec<u8> = vec![0; (width * height) as usize];
+    let mut grid: Vec<u8> = vec![0; (WIDTH * HEIGHT) as usize];
     for seconds in 0.. {
         grid.fill(b' ');
         let mut good = true;
         for (p, v) in robots.iter() {
             let (x, y) = (
-                (p.0 + seconds * v.0).rem_euclid(width),
-                (p.1 + seconds * v.1).rem_euclid(height),
+                (p.0 + seconds * v.0).rem_euclid(WIDTH),
+                (p.1 + seconds * v.1).rem_euclid(HEIGHT),
             );
-            let index = (y * width + x) as usize;
+            let index = (y * WIDTH + x) as usize;
             if grid[index] != b' ' {
                 good = false;
                 break;
@@ -64,8 +60,8 @@ fn main() {
             grid[index] = b'O';
         }
         if good {
-            for index in (0..grid.len()).step_by(width as usize) {
-                let row = &grid[index..index + width as usize];
+            for index in (0..grid.len()).step_by(WIDTH as usize) {
+                let row = &grid[index..index + WIDTH as usize];
                 println!("{}", str::from_utf8(row).unwrap());
             }
             ans2 = seconds;
