ce7a71885765e6df4b27d911ce3d81aaa52750ed
nightuser <nightuser.android@gmail.com>
2024-12-23 00:07:00 +0000

    [day22] Add naive solution

---
+++ day22/src/main.rs
@@ -0,0 +1,63 @@
+use std::env;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+const MASK: i32 = 0xfffff;
+
+fn next_secret_number(mut x: u64) -> u64 {
+    x = ((x * 64) ^ x) % 16777216;
+    x = ((x / 32) ^ x) % 16777216;
+    x = ((x * 2048) ^ x) % 16777216;
+    x
+}
+
+fn main() {
+    let input = env::args_os().nth(1).unwrap();
+    let reader = BufReader::new(File::open(input).unwrap());
+    let mut inputs: Vec<u64> = Vec::new();
+    for line in reader.lines() {
+        let line = line.unwrap();
+        inputs.push(line.parse().unwrap());
+    }
+
+    let mut ans1: u64 = 0;
+    for &(mut input) in inputs.iter() {
+        for _ in 0..2000 {
+            input = next_secret_number(input);
+        }
+        ans1 += input;
+    }
+    println!("ans1 = {ans1}");
+
+    let mut storage: Vec<i32> = vec![0; 1 << 20];
+    let mut seen: Vec<bool> = vec![false; 1 << 20];
+    for &(mut input) in inputs.iter() {
+        seen.fill(false);
+        let mut value = 0;
+        let mut prev = (input % 10) as i32;
+
+        for _ in 0..3 {
+            input = next_secret_number(input);
+            let current = (input % 10) as i32;
+            let diff = (current - prev) + 10;
+            value = (value << 5) | diff;
+            prev = current;
+        }
+
+        for _ in 0..1997 {
+            input = next_secret_number(input);
+            let current = (input % 10) as i32;
+            let diff = (current - prev) + 10;
+            value = ((value << 5) & MASK) | diff;
+            let value_index = value as usize;
+            if !seen[value_index] {
+                storage[value_index] += current;
+            }
+            seen[value_index] = true;
+            prev = current;
+        }
+        ans1 += input;
+    }
+    let ans2 = storage.into_iter().max().unwrap();
+    println!("ans2 = {ans2}");
+}
